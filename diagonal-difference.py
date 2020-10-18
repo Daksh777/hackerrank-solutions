919670b925b79b1e0af9370199fc053c3b6478a2
Daksh P. Jain <contact@daksh.eu.org>
2020-10-18 10:58:52 +0530

    Create diagonal-difference.py

---
+++ diagonal-difference.py
@@ -0,0 +1,8 @@
+size = int(input())
+nums = []
+for i in range(size):
+    nums.append([int(x) for x in input().split()])
+sum1 = sum(nums[i][i] for i in range(size))
+sum2 = sum(nums[i][size-i-1] for i in range(size))
+ans = abs(sum1 - sum2)
+print(ans)
