c2637d31f61c2fd4651fa688f0c91e633d6b565d
Daksh P. Jain <contact@daksh.eu.org>
2020-10-19 21:15:12 +0530

    Update diagonal-difference.py

--- diagonal-difference.py
+++ diagonal-difference.py
@@ -3,6 +3,6 @@ nums = []
 for i in range(size):
     nums.append([int(x) for x in input().split()])
 sum1 = sum(nums[i][i] for i in range(size))
-sum2 = sum(nums[i][size-i-1] for i in range(size))
+sum2 = sum(nums[i][-i-1] for i in range(size))
 ans = abs(sum1 - sum2)
 print(ans)
